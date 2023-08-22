3931d94a396c798924c4d12674e7713749dd2273
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2023-08-22 03:49:13 -0700

    0x11. C - printf

--- get_precision.c
+++ get_precision.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
- */
+* get_precision - Calculates the precision for printing
+* @format: Formatted string in which to print the arguments
+* @i: List of arguments to be printed
+* @list: list of arguments
+*
+* Return: Precision.
+*/
 
 int get_precision(const char *format, int *i, va_list list)
 {
@@ -16,6 +16,7 @@ int get_precision(const char *format, int *i, va_list list)
 
 	if (format[curr_i] != '.')
 		return (precision);
+
 	precision = 0;
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
@@ -34,6 +35,7 @@ int get_precision(const char *format, int *i, va_list list)
 		else
 			break;
 	}
+
 	*i = curr_i - 1;
 
 	return (precision);
